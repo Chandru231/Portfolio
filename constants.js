c13384bef1961dc2440b68acf25ad499ac577d3e
Chandru <chandruu231@gmail.com>
2025-12-24 18:23:46 +0530

    .

--- constants.js
+++ constants.js
@@ -6,7 +6,7 @@ export const PROJECTS = [
         techStack: ['REACT JS', 'JAVASCRIPT', 'HTML', 'CSS'],
         imageUrl: './images/Landing page thumbnail.jpg',
         year: '2024',
-        liveDemo: 'https://github.com/Chandru231?tab=repositories'
+        liveDemo: 'https://chandru231.github.io/Landing-page-showcase/'
     },
     {
         id: 'p2',
